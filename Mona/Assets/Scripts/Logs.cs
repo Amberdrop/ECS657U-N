5100200af9c6f6be4f8f50fea96cb4a826eb3cf8
Amberdrop <78417261+Amberdrop@users.noreply.github.com>
2023-01-04 06:43:30 +0000

    Changed the logs a bit

--- Mona/Assets/Scripts/Logs.cs
+++ Mona/Assets/Scripts/Logs.cs
@@ -5,7 +5,7 @@
     Finally landed on our new home - been awake now for around 3 hours - cryosleep really does a number on your knees!
     Still, better than spending 3 years sitting around on a cramped ship. I can't believe we are finally here.
     The planet is beautiful - full of grass and trees (Real trees! I havn't seen those since I was just a kid!).
-    The island we are one is big enough for our plans, and we still have the terraaformer if we need to expand.
+    The island we are one is big enough for our plans, and we still have the terraaformer if we need to expand, and the new state-of0+-the-art drones in orbit, ready to keep us happy and healthy no matter what!
     Guess it's time to go meet my new neighbours!
 
     .S
@@ -30,6 +30,8 @@
     "Log 4; 28/5/2043
 
     K is dead. The medical drone finally came out and told us. It won't say how, and it won't let the doctor, P, in to check the body. She was absolutely fine, and a broken leg is basically nothing to the meds even we have in the med kit.
-    I don't trust the drones. I need to find out whats in the storage shed. I foud the code - its 9427. That should let me in.
+    I don't trust the drones. I need to find out whats in the storage shed, what the drones are up to. I know the control center for them is in there. I foud the code - its 9427. That should let me in. Wish me luck.
+
+    If anyone finds these logs, it means I failed. Please - destroy the drones before they can do any more harm. 
     "
 
