1053a978ac65b1743687c2e72e83bc2a20bf8014
Sohee Kim <kim66s@mtholyoke.edu>
2023-01-09 09:31:31 +0900

    fix bug

--- Mona/Assets/Scripts/PlayerController.cs
+++ Mona/Assets/Scripts/PlayerController.cs
@@ -31,7 +31,7 @@ public class PlayerController : MonoBehaviour {
     void Start () {
         GemCount = 0;
         waterMove = false;
-        SetGemCountText();
+        // SetGemCountText();
         mapOpen = false;
 
         if (PlayerPrefs.HasKey("speed")) {
@@ -246,7 +246,7 @@ public class PlayerController : MonoBehaviour {
         // diary 4 -> update 6 index
 
         int index = (diary - 1) * 2;
-        s = s.Remove(index,index+1);
+        s = s.Remove(index,1);
         s = s.Insert(index,"1");
 
         PlayerPrefs.SetString("diaries", s);
